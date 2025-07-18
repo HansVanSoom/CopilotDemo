3c005d87158b0503d30b3eb157eb8bb9d68451e8
Hans Van Soom <Hans.VanSoom@codit.eu>
2025-07-18 13:34:39 +0200

    Using an environment variable instead of a hardcoded Azure Storage key.

--- CopilotDemo/Program.cs
+++ CopilotDemo/Program.cs
@@ -7,9 +7,16 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddLogging(); // Registers logging
 
+// Get storage key from environment variable
+var storageKey = Environment.GetEnvironmentVariable("AZURE_STORAGE_KEY");
+if (string.IsNullOrWhiteSpace(storageKey))
+{
+    throw new InvalidOperationException("AZURE_STORAGE_KEY not set in environment variables.");
+}
+
 // Register TableClient and UserHashService with DI
 builder.Services.AddSingleton<TableClient>(sp =>
-    new TableClient("REDACTED", "UserHashes"));
+    new TableClient($"DefaultEndpointsProtocol=https;AccountName=storageaccounthvs2;AccountKey={storageKey};EndpointSuffix=core.windows.net", "UserHashes"));
 builder.Services.AddScoped<UserHashService>();
 
 var app = builder.Build();
